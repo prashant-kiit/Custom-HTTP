3e36726b86fb33e5de6bf512cad67960408b9901
prashant-nsws <prashant.singh@neurosparkworks.com>
2025-01-11 16:05:19 +0530

    feat: added error print

--- frontend/src/Client.java
+++ frontend/src/Client.java
@@ -1,4 +1,3 @@
-import backend.src.application.database.User;
 import frontend.ENV;
 import protocol.HTTP;
 import protocol.Response;
@@ -11,29 +10,32 @@ public class Client {
             Response response1 = HTTP.get(ENV.BASE_URL + "/users");
             System.out.println("Code = " + response1.getCode() +
                     ", Message = " + response1.getMessage() +
-                    ", Data = " + response1.getData());
+                    ", Data = " + response1.getData() +
+                    ", Error = " + response1.getError());
 
-            Response response2 = HTTP.get(ENV.BASE_URL + "/users/John");
-            System.out.println("Code = " + response2.getCode() +
-                    ", Message = " + response2.getMessage() +
-                    ", Data = " + response2.getData());
+            // Response response2 = HTTP.get(ENV.BASE_URL + "/users/John");
+            // System.out.println("Code = " + response2.getCode() +
+            // ", Message = " + response2.getMessage() +
+            // ", Data = " + response2.getData() +
+            // ", Error = " + response2.getError());
 
-            Response response3 = HTTP.post(ENV.BASE_URL + "/user", new User("Prashant",
-                    "password", "j@j.com"));
-            System.out.println("Code = " + response3.getCode() +
-                    ", Message = " + response3.getMessage() +
-                    ", Data = " + response3.getData());
+            // Response response3 = HTTP.post(ENV.BASE_URL + "/user", new User("Prashant",
+            // "password", "j@j.com"));
+            // System.out.println("Code = " + response3.getCode() +
+            // ", Message = " + response3.getMessage() +
+            // ", Error = " + response3.getError());
 
-            Response response4 = HTTP.put(ENV.BASE_URL + "/users/John", new User("John",
-                    "password", "john@john.com"));
-            System.out.println("Code = " + response4.getCode() +
-                    ", Message = " + response4.getMessage() +
-                    ", Data = " + response4.getData());
+            // Response response4 = HTTP.put(ENV.BASE_URL + "/users/John", new User("John",
+            // "password", "john@john.com"));
+            // System.out.println("Code = " + response4.getCode() +
+            // ", Message = " + response4.getMessage() +
+            // ", Error = " + response4.getError());
 
-            Response response5 = HTTP.delete(ENV.BASE_URL + "/users/John");
-            System.out.println("Code = " + response5.getCode() +
-                    ", Message = " + response5.getMessage() +
-                    ", Data = " + response5.getData());
+            // Response response5 = HTTP.delete(ENV.BASE_URL + "/users/Joh");
+            // System.out.println("Code = " + response5.getCode() +
+            // ", Message = " + response5.getMessage() +
+            // ", Data = " + response5.getData() +
+            // ", Error = " + response5.getError());
         } catch (Exception ex) {
             System.out.println("Exception: " + ex.getMessage());
             ex.printStackTrace();
