d5218439b94923935038865d9b78dc42a2101d13
prashant-nsws <prashant.singh@neurosparkworks.com>
2025-01-18 23:01:05 +0530

    test: singleton with multithreading

---
+++ test/Main.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Main {
+    public static void main(String[] args) {
+        new Thread(new Process("Thread 1")).start();
+        new Thread(new Process("Thread 2")).start();
+    }
+}
