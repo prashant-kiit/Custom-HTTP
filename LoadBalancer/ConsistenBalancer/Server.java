af2ee8cd7eda28aff3487f7bb2996ad773ffd8ef
prashant-nsws <prashant.singh@neurosparkworks.com>
2025-02-06 16:04:44 +0530

    feat: added server in consistent server

---
+++ LoadBalancer/ConsistenBalancer/Server.java
@@ -0,0 +1,13 @@
+package ConsistenBalancer;
+
+public class Server {
+    private Boolean isActive;
+
+    public Server(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+}
